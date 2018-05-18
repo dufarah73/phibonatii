ba5033957af509d903cb917c4d172525095bc976
dufarah73 <eduardo@incorptech.com.br>
2018-05-18 09:56:39 -0300

    usando spinner e tablayout

--- app/src/main/java/br/com/phibonatii/phibonatii/MainActivity.java
+++ app/src/main/java/br/com/phibonatii/phibonatii/MainActivity.java
@@ -30,37 +30,27 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-    private ViewPager mViewPager;
+    private Context myContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myContext = this;
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        // Setup spinner
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        spinner.setAdapter(new MyAdapter(
-                toolbar.getContext(),
-                new String[]{
-                        "RADAR",
-                        "RANKING",
-                        "BONA",
-                }));
-
-        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new String[]{"RADAR","RANKING","BONA"}));
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                // When the given dropdown item is selected, show its contents in the
-                // container view.
-                TextView tv = (TextView) view;
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.container, PlaceholderFragment.newInstance( tv.getText().toString() ))
-                        .commit();
+                TabLayout tb = (TabLayout) findViewById(R.id.tabs);
+                int myTabPosition = tb.getSelectedTabPosition();
+                int mySpinnerPosition = position;
+                Toast.makeText(myContext, "spi -> sp:"+String.valueOf(mySpinnerPosition)+",tb:"+String.valueOf(myTabPosition), Toast.LENGTH_LONG).show();
             }
 
             @Override
@@ -68,26 +58,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-////////////////////////
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Spinner sp = (Spinner) findViewById(R.id.spinner);
+                int mySpinnerPosition = sp.getSelectedItemPosition();
+                int myTabPosition = tab.getPosition();
+                Toast.makeText(myContext, "tab -> sp:"+String.valueOf(mySpinnerPosition)+",tb:"+String.valueOf(myTabPosition), Toast.LENGTH_LONG).show();
+            }
 
-        mViewPager = (ViewPager) findViewById(R.id.container);
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
 
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount(), spinner);
-
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
-/////////////////////////
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+        tabLayout.addTab(tabLayout.newTab().setText("GLOBAL"));
+        tabLayout.addTab(tabLayout.newTab().setText("FAMILIAX"));
+        tabLayout.addTab(tabLayout.newTab().setText("ESCOLAX"));
+        tabLayout.addTab(tabLayout.newTab().setText("TRABALHOX"));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
 
@@ -118,100 +116,4 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-    private static class MyAdapter extends ArrayAdapter<String> implements ThemedSpinnerAdapter {
-        private final ThemedSpinnerAdapter.Helper mDropDownHelper;
-
-        public MyAdapter(Context context, String[] objects) {
-            super(context, android.R.layout.simple_list_item_1, objects);
-            mDropDownHelper = new ThemedSpinnerAdapter.Helper(context);
-        }
-
-        @Override
-        public View getDropDownView(int position, View convertView, ViewGroup parent) {
-            View view;
-
-            if (convertView == null) {
-                // Inflate the drop down using the helper's LayoutInflater
-                LayoutInflater inflater = mDropDownHelper.getDropDownViewInflater();
-                view = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
-            } else {
-                view = convertView;
-            }
-
-            TextView textView = (TextView) view.findViewById(android.R.id.text1);
-            textView.setText(getItem(position));
-
-            return view;
-        }
-
-        @Override
-        public Theme getDropDownViewTheme() {
-            return mDropDownHelper.getDropDownViewTheme();
-        }
-
-        @Override
-        public void setDropDownViewTheme(Theme theme) {
-            mDropDownHelper.setDropDownViewTheme(theme);
-        }
-    }
-
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(String sectionName) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putString(ARG_SECTION_NUMBER, sectionName);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getArguments().getString(ARG_SECTION_NUMBER));
-            return rootView;
-        }
-    }
-
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        private int tabCount;
-        private Spinner spinner;
-
-        public SectionsPagerAdapter(FragmentManager fm, int tc, Spinner spi) {
-            super(fm);
-            tabCount = tc;
-            spinner = spi;
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            TextView tv = (TextView) spinner.getSelectedView();
-            return PlaceholderFragment.newInstance(String.valueOf(position)+";"+tv.getText().toString());
-        }
-
-        @Override
-        public int getCount() {
-            return tabCount;
-        }
-    }
-
 }
