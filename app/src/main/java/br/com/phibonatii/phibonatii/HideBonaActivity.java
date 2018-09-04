e31d7c392ca64e6cfcc5149345d37b779a8e4316
dufarah73 <eduardo@incorptech.com.br>
2018-09-04 15:54:25 -0300

    tratando upload imagens

--- app/src/main/java/br/com/phibonatii/phibonatii/HideBonaActivity.java
+++ app/src/main/java/br/com/phibonatii/phibonatii/HideBonaActivity.java
@@ -1,29 +1,52 @@
 package br.com.phibonatii.phibonatii;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.LocationServices;
 
 import java.io.File;
+import java.io.IOException;
+
+import net.gotev.uploadservice.MultipartUploadRequest;
+import net.gotev.uploadservice.UploadNotificationConfig;
+
+import java.util.UUID;
 
 public class HideBonaActivity extends AppCompatActivity {
 
-    public static final int CODIGO_CAMERA = 567;
+    private static final String UPLOAD_URL = "http://192.168.94.1/AndroidImageUpload/upload.php";
+    private static final int CODIGO_IMAGEM = 345;
+    private static final int CODIGO_CAMERA = 567;
     private String caminhoFoto;
+    public EditText fieldDescription;
+    public String description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hide_bona);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE ) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 456);
+        }
 
         FragmentManager fragMan = getSupportFragmentManager();
         FragmentTransaction fragTrans = fragMan.beginTransaction();
@@ -31,6 +54,57 @@ public class HideBonaActivity extends AppCompatActivity {
         fragTrans.commit();
     }
 
+    public void uploadMultipart(View v) {
+        try {
+            fieldDescription = (EditText) this.findViewById(R.id.edit_description);
+            description = fieldDescription.getText().toString();
+            String uploadId = UUID.randomUUID().toString();
+            new MultipartUploadRequest(this, uploadId, UPLOAD_URL)
+                    .addFileToUpload(caminhoFoto, "image")
+                    .addParameter("description", description)
+                    .setNotificationConfig(new UploadNotificationConfig())
+                    .setMaxRetries(2)
+                    .startUpload(); //Starting the upload
+
+        } catch (Exception exc) {
+            Toast.makeText(this, exc.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void capturePhoto(View v) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Selecione Foto"), CODIGO_IMAGEM);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CODIGO_IMAGEM && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+
+            Cursor cursor = getContentResolver().query(filePath, null, null, null, null);
+            cursor.moveToFirst();
+            String document_id = cursor.getString(0);
+            document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
+            cursor.close();
+            cursor = getContentResolver().query(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,null,MediaStore.Images.Media._ID + " = ? ", new String[]{document_id},null);
+            cursor.moveToFirst();
+            caminhoFoto = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            cursor.close();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                ImageView imageView = (ImageView) this.findViewById(R.id.formulario_foto);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+/*
     public void capturePhoto(View v) {
         Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         caminhoFoto = getExternalFilesDir(null) + "/" + System.currentTimeMillis() + ".jpg";
@@ -54,4 +128,6 @@ public class HideBonaActivity extends AppCompatActivity {
             }
         }
     }
+*/
+
 }
